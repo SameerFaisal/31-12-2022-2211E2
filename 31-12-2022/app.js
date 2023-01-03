d69d11e20311ef4750f2d77c0d6e6efb5a1a0f24
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-03 17:09:11 +0500

    31-12-2022

---
+++ 31-12-2022/app.js
@@ -0,0 +1,108 @@
+// var age=+prompt("Enter your age?")
+// alert(typeof(age))
+
+// var firstNumber=prompt("Enter first number")
+// var secondNumber=prompt("Enter second number")
+
+// var sum=parseFloat(firstNumber)/parseFloat(secondNumber)
+
+// alert(sum)
+
+// var result=12/4+(3*6)/3-26*(16-5)
+
+// alert(result)
+
+// var length=prompt("Enter length of rectangle")
+// var breadth=prompt("Enter breadth of rectangle")
+
+// var area=parseFloat(length)*parseFloat(breadth)
+
+// document.write(area)
+
+
+// var length=prompt("Enter length of rectangle")
+// var breadth=prompt("Enter breadth of rectangle")
+
+// var perimeter=2*(parseFloat(length)+parseFloat(breadth))
+
+// document.write(perimeter)
+
+// var pkr=prompt("Enter any amount in pkr")
+// var usd=parseFloat(pkr)/226.55
+// document.write(usd)
+
+// var usd=prompt("Enter any amount in usd")
+// var pkr=parseFloat(usd)*226.55
+// document.write(pkr)
+
+
+// var num=1
+// alert(++num)//Increment Operator
+
+// num++ //Post Increment
+// ++num //Pre Increment
+
+
+// var num=1
+// alert(num++)
+// alert(num++)
+// alert(num++)
+
+
+// var num=1
+// alert(++num)
+// alert(++num)
+// alert(++num)
+
+// --num //Pre Decrement
+// num-- //Post Decrement
+
+// var num=1
+// alert(--num) //0
+// alert(++num) //1
+// alert(++num) //2
+// alert(num--) //2
+// alert(num++) //1
+// alert(num++) //2
+// alert(--num) //1
+// alert(++num) //2
+// alert(num++) //2
+
+// var num=1
+// alert(num)
+// num=num+2
+// alert(num)
+// num=num-3
+// alert(num)
+// num=num+4
+// alert(num)
+// num=num-2
+// alert(num)
+
+// document.write(10<=10)
+
+// document.write(10<="10")
+
+// document.write(2!=="2") //NOT
+
+// document.write(10!=="10")
+
+// var length=prompt("Enter length of rectangle")
+// var breadth=prompt("Enter breadth of rectangle")
+
+// var perimeter=2*(parseFloat(length)+parseFloat(breadth))
+
+// // document.write("<h1>The perimeter is: </h1>"+perimeter)
+// document.write("The perimeter is: "+perimeter+" cm")
+
+
+
+
+
+
+
+
+
+
+
+
